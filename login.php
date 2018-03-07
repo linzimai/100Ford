48ce46e55735dcd51c17cc3a048bf9717a5e9ad9
Linzi Gould <g1730119@student.southstaffs.ac.uk>
2018-03-07 16:14:51 +0000

    changes

--- login.php
+++ login.php
@@ -95,8 +95,6 @@
           <input id="icon_prefix" type="text" class="validate">
           <label for="icon_prefix">Username</label>
         </div>
-        </div>
-        <div class="row">
         <div class="input-field col s6">
           <i class="material-icons prefix">contact_mail</i>
           <input id="icon_telephone" type="tel" class="validate">
@@ -104,7 +102,7 @@
         </div>
         </div>
         <div class="row">
-          <div class="input-field col s6">
+          <div class="input-field col s12">
           <i class="material-icons prefix">chat_bubble</i>
           <input id="icon_telephone" type="tel" class="validate">
           <label for="icon_telephone">Password</label>
